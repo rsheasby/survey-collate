f69a0861c824bcfc67d19f576d5399211de5b578
Ryan David Sheasby <ryan@sheasby.dev>
2021-06-09 00:18:11 +0200

    Add question loading and navigation

--- src/Collation.svelte
+++ src/Collation.svelte
@@ -3,11 +3,65 @@
     import type { Question } from './Question';
 
     export let questions: Question[];
+    let currentQuestionNumber: number = 0;
+    $: currentQuestion = questions[currentQuestionNumber];
+    $: canGoLeft = currentQuestionNumber > 0;
+    $: canGoRight = currentQuestionNumber < (questions.length - 1);
 
+    function goLeft() {
+        if (canGoLeft)
+            currentQuestionNumber--;
+    }
+
+    function goRight() {
+        if (canGoRight)
+            currentQuestionNumber++;
+    }
 </script>
 
-<div class="card">
+<div id="collate-card" class="card">
+    <div id="collate-card-header" class="card-header gap-3 fs-2">
+        <div>
+            <button class="nav-button" class:text-secondary={!canGoLeft} disabled={!canGoLeft} on:click={goLeft}>
+                <i class="far fa-arrow-alt-circle-left"></i>
+            </button>
+        </div>
+        <div id="current-question-text-container">
+            {currentQuestion.text}
+        </div>
+        <div>
+            <button class="nav-button" class:text-secondary={!canGoRight} disabled={!canGoRight} on:click={goRight}>
+                <i class="far fa-arrow-alt-circle-right"></i>
+            </button>
+        </div>
+    </div>
     <div class="card-body">
-        {questions.length}
     </div>
 </div>
+
+<style>
+    .nav-button {
+        line-height: 100%;
+        border-radius: 50%;
+        padding: 0;
+        margin: 0;
+    }
+
+    #collate-card {
+        max-width: 600px;
+        width: 100%;
+    }
+
+    #collate-card-header {
+        width: 100%;
+        display: grid;
+        grid-auto-flow: column;
+        grid-template-columns: auto 1fr auto;
+        align-items: center;
+    }
+
+    #current-question-text-container {
+        display: flex;
+        justify-content: center;
+    }
+</style>
